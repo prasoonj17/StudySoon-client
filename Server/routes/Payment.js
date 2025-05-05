c2978f4f3fed0067fceeebda55e7a1d58cd28826
prasoonj17 <159968472+prasoonj17@users.noreply.github.com>
2025-05-05 11:13:35 +0530

    first commit

--- Server/routes/Payment.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const router=express.Router()
-
-const {capturePayment,verifySignature}=require("../controllers/Payments");
-const{auth,isStudent,isInstructor,isAdmin}=require("../middlewares/auth")
-router.post("/capturePayment",auth,isStudent,capturePayment)
-router.post("/verifySignature",auth,isStudent,verifySignature);
-module.exports=router
